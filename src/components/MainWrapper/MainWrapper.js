db0f1c04fe7eefa421fd25b55d990a82eaea6657
slawo tuszko <odzeradodeveloperajava@gmail.com>
2022-10-29 20:54:40 +0200

    refactor to absloute import path

--- src/components/MainWrapper/MainWrapper.js
+++ src/components/MainWrapper/MainWrapper.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Cats from 'components/Cats/Cats.js'
 import Header from 'components/Header/Header.js'
+import Cats from 'components/Cats/Cats.js'
 import 'components/MainWrapper/MainWrapper.css'
 
 const MainWrapper = () => {
